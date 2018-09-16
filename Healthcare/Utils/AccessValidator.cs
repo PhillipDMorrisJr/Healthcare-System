4c715dbf1a2a78b11f50bca1f5b9701eec9ca804
Phillip Morris <pmorris4@my.westga.edu>
2018-09-16 00:03:56 -0400

    Can now log in and out

---
+++ Healthcare/Utils/AccessValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    public static class AccessValidator
+    {
+        /// <summary>
+        /// Confirms the user access by checking to see if user exists inside database.
+        /// </summary>
+        /// <param name="username">The username.</param>
+        /// <param name="password">The password.</param>
+        /// <returns></returns>
+        public static bool ConfirmUserAccess(String username, string password)
+        {
+            return true;
+        }
+
+    }
+}
