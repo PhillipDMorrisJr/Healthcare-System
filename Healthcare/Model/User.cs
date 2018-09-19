d59df1f6432d20dda955b55c47f89bc61402f87c
pmorris4 <csuser@CS1115W02>
2018-09-18 21:10:37 -0400

    Sample usernames and passwords implemented

---
+++ Healthcare/Model/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Healthcare.Model
+{
+    public class User
+    {
+        public string Username { get;}
+        public string Password { get; }
+
+        public User(string username, string password)
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+    }
+}
--- Healthcare/Utils/AccessValidator.cs
+++ Healthcare/Utils/AccessValidator.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Notifications;
+using Healthcare.Model;
 
 namespace Healthcare.Utils
 {
@@ -7,6 +10,21 @@ namespace Healthcare.Utils
     /// </summary>
     public static class AccessValidator
     {
+        
+        private static List<User> users = new List<User>()
+        {
+            (new User("Phillip", "Phillycheeze101")),
+            (new User("Caviaye", "Password0")),
+            (new User("Dr. Yang", "Password")),
+            (new User("1", "1")),
+            (new User("2", "2")),
+            (new User("2", "2")),
+        };
+
+
+
+
+
         /// <summary>
         /// Confirms the user access by checking to see if user exists inside database.
         /// </summary>
@@ -15,11 +33,15 @@ namespace Healthcare.Utils
         /// <returns></returns>
         public static bool ConfirmUserAccess(String username, string password)
         {
-            return !(string.IsNullOrWhiteSpace(username) && string.IsNullOrWhiteSpace(password));
+            bool validInput = ValidateThatUsernameAndPasswordAreNotEmpty(username, password);
+            bool validUser = users.Exists(user => user.Username == username && user.Password == password);
+            return validUser && validInput;
 
         }
 
-        
-
+        private static bool ValidateThatUsernameAndPasswordAreNotEmpty(string username, string password)
+        {
+            return !(string.IsNullOrWhiteSpace(username) && string.IsNullOrWhiteSpace(password));
+        }
     }
 }
