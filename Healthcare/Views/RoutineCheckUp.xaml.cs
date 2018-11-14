35756f492fd52ad0116fa488f1283c085528e717
pmorris4 <pmorris4@my.westga.edu>
2018-11-13 22:47:29 -0500

    Check up page validated ready for db

--- Healthcare/Views/RoutineCheckUp.xaml.cs
+++ Healthcare/Views/RoutineCheckUp.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -29,47 +30,83 @@ namespace Healthcare.Views
 
         private void checkup_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            bool hasNull = systolic.Text == null || diastolic.Text == null || temperature.Text == null ||
+                           pulse.Text == null || weight.Text == null;
+            if (!hasNull)
+            {
+                this.Frame.Navigate(typeof(MainPage));
+            }
         }
 
         private void home_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            bool hasNull = systolic.Text == null || diastolic.Text == null || temperature.Text == null ||
+                           pulse.Text == null || weight.Text == null;
+            if (!hasNull)
+            {
+this.Frame.Navigate(typeof(MainPage));
+            }
+            
         }
 
         private void systolic_TextChanged(object sender, TextChangedEventArgs e)
         {
+            if (isThreeDigits(systolic.Text))
+            {
+                systolic.Text = "";
+            }
+        }
 
+        private bool isThreeDigits(string text)
+        {
+            return !Regex.IsMatch(text, "^(.*[^0-9]|)(1000|[1-9]\\d{0,2})([^0-9].*|)$") || text.Any(character => char.IsLetter(character));
         }
 
         private void diastolic_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            if (isThreeDigits(diastolic.Text))
+            {
+                diastolic.Text = "";
+            }
         }
 
         private void weight_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            if (isThreeDigits(weight.Text))
+            {
+                weight.Text = "";
+            }
         }
 
         private void pulse_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            if (isThreeDigits(pulse.Text))
+            {
+                pulse.Text = "";
+            }
         }
 
         private void temperature_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            if (isThreeDigits(temperature.Text))
+            {
+                int temp = Int32.Parse(temperature.Text);
+                if (temp > 0 && temp < 200)
+                {
+                    temperature.Text = "";
+                }
+                
+            }
         }
 
         private void addSymptom_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+
         }
 
         private void removeSymptom_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+           
         }
     }
 }
