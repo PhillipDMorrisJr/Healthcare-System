13287ce05653d1cace50ae4a106ac6e9e0dbb7a5
pmorris4 <pmorris4@my.westga.edu>
2018-11-28 09:28:28 -0500

    QueryPAge fully functional

---
+++ Healthcare/DAL/QueryDAL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Healthcare.DAL
+{
+    public static class QueryDAL
+    {
+        public static DataTable GetResults(string query)
+        {
+
+            using (MySqlConnection conn = DbConnection.GetConnection())
+            {
+                conn.Open();
+                using (var cmd = new MySqlCommand(query, conn))
+                {
+
+                    DataTable dataTable = new DataTable();
+
+                    using (var dataAdapter = new MySqlDataAdapter(cmd))
+                    {
+                        dataAdapter.Fill(dataTable);
+                        return dataTable;
+                    }
+                }
+            }
+        }
+    }
+}
--- Healthcare/Views/QueryPage.xaml.cs
+++ Healthcare/Views/QueryPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -12,16 +14,20 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Healthcare.Utils;
+using Microsoft.Toolkit.Uwp.UI.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Healthcare.Views
 {
+    
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class QueryPage : Page
     {
+        private List<List<object>> o;
         public QueryPage()
         {
             this.InitializeComponent();
@@ -34,7 +40,22 @@ namespace Healthcare.Views
 
         private void query_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            DataTable table = CustomQuery.RetrieveResults(this.query.Text);
+            for (int i = 0; i < table.Columns.Count; i++)
+            {
+                Results.Columns.Add(new DataGridTextColumn()
+                {
+                    Header = table.Columns[i].ColumnName,
+                    Binding = new Binding { Path = new PropertyPath("[" + i.ToString() + "]") }
+                });
+            }
+            var collection = new ObservableCollection<object>();
+            foreach (DataRow row in table.Rows)
+            {
+                collection.Add(row.ItemArray);
+            }
+            Results.ItemsSource = collection;
+          
         }
 
         private void home_Click(object sender, RoutedEventArgs e)
